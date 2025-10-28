ea642c1cfdc4f4aebbffa800dd39c427fe5846fb
AnkitaAR <67515597+AnkitaAR@users.noreply.github.com>
2025-10-28 08:14:09 +0530

    Add files via upload

---
+++ nested conditions/checkage.py
@@ -0,0 +1,7 @@
+age=int(input("Enter your age whether it is between 10-20years :"))
+if age>=10 and age<=20:
+    print("Yes your age lies between 10years to 20 years")
+elif age<10:
+    print("You are too young")
+elif age>20:
+    print("You are older than 20")
